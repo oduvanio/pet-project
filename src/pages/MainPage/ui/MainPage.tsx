df27954e641b8b04aa13dff04cef903731f0f137
oduvanio <oduvanio@mail.ru>
2025-01-23 00:24:11 +0400

    input login

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,16 +1,17 @@
-import { BugButton } from 'app/providers/ErrorBoundary';
-import { Counter } from 'entities/Counter';
-import React from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
     const { t } = useTranslation();
+    const [value, setValue] = useState('');
+
+    const onChange = (val: string) => {
+        setValue(val);
+    };
 
     return (
         <div>
-            <BugButton />
             {t('Главная страница')}
-            <Counter />
         </div>
     );
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { useCallback, useState } from 'react';
 import { Modal } from 'shared/ui/Modal/Modal';
+import { LoginModal } from 'features/AuthByUsername';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -13,8 +14,12 @@ export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation('about');
     const [isAuthModal, setIsAuthModal] = useState(false);
 
-    const onToggleModal = useCallback(() => {
-        setIsAuthModal((prev) => !prev);
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false);
+    }, []);
+
+    const onShowModal = useCallback(() => {
+        setIsAuthModal(true);
     }, []);
 
     return (
@@ -22,14 +27,14 @@ export const Navbar = ({ className }: NavbarProps) => {
             <Button
                 theme={ButtonTheme.CLEAR_INVERTED}
                 className={cls.links}
-                onClick={onToggleModal}
+                onClick={onShowModal}
             >
                 {t('Войти')}
             </Button>
-            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-                {/* eslint-disable-next-line */}
-                {t('Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti omnis ab ipsa at enim error ipsum, incidunt iure necessitatibus quia odit dolor animi praesentium sapiente facilis et quas culpa eaque, debitis rem possimus sequi! Repellendus aperiam doloremque itaque voluptatum consequatur.')}
-            </Modal>
+            <LoginModal
+                isOpen={isAuthModal}
+                onClose={onCloseModal}
+            />
         </div>
     );
 };

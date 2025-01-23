e4c5f4052ba67846a1904955cca7a7f30eb8e2b7
oduvanio <oduvanio@mail.ru>
2025-01-23 17:50:29 +0400

    fix storybook and add pre-commit husky

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { LoginForm } from './LoginForm';
+
+const meta: Meta<typeof LoginForm> = {
+    component: LoginForm,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof LoginForm>
+
+export const Primary: Story = {
+    decorators: [],
+    name: 'Primary',
+    argTypes: {},
+    args: {},
+};
--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -7,7 +7,6 @@ const AboutPage = () => {
     return (
         <div>
             {t('О сайте')}
-            <Counter />
         </div>
     );
 };

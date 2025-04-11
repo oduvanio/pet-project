db1a869e92bbc2c950805e8b24e9bc09b4cefa84
oduvanio <oduvanio@mail.ru>
2025-04-11 22:13:52 +0400

    add fetching articles, view type selector

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
@@ -3,7 +3,7 @@ import { ComponentType, lazy } from 'react';
 export const ArticleDetailsPageAsync = lazy(async () => {
     const result = await new Promise<{default: ComponentType<any>}>((resolve) => {
         // @ts-ignore
-        setTimeout(() => { resolve(import('./ArticleDetailsPage')); }, 1500);
+        setTimeout(() => { resolve(import('./ArticleDetailsPage')); }, 400);
     });
     return result;
 });
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
@@ -3,7 +3,7 @@ import { ComponentType, lazy } from 'react';
 export const ArticlesPageAsync = lazy(async () => {
     const result = await new Promise<{default: ComponentType<any>}>((resolve) => {
         // @ts-ignore
-        setTimeout(() => { resolve(import('./ArticlesPage')); }, 1500);
+        setTimeout(() => { resolve(import('./ArticlesPage')); }, 400);
     });
     return result;
 });

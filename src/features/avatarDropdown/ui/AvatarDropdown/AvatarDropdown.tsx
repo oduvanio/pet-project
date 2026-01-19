e758820ee3dec645b4fe757d841e953809dbb798
oduvanio <oduvanio@mail.ru>
2026-01-19 14:35:14 +0400

    refactoring routing

--- src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
@@ -8,7 +8,7 @@ import {
 } from '@/entities/User';
 import { Avatar } from '@/shared/ui/Avatar';
 import cls from './AvatarDropdown.module.scss';
-import { RoutePath } from '@/shared/const/router';
+import { getRouteAdminPanel, getRouteProfile } from '@/shared/const/router';
 
 interface AvatarDropdownProps {
     className?: string;
@@ -39,11 +39,11 @@ export const AvatarDropdown = memo((props: AvatarDropdownProps) => {
             items={[
                 ...(isAdminPanelAvailable ? [{
                     content: t('Админка'),
-                    href: RoutePath.admin_panel,
+                    href: getRouteAdminPanel(),
                 }] : []),
                 {
                     content: t('Профиль'),
-                    href: RoutePath.profile + authData.id,
+                    href: getRouteProfile(authData.id),
                 },
                 {
                     content: t('Выйти'),

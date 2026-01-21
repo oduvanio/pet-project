294dff0790a28146e0bcc52b9292beda9d47d714
oduvanio <oduvanio@mail.ru>
2026-01-21 15:16:25 +0400

    update react testing library, add tests for router

--- src/pages/AdminPanelPage/ui/AdminPanelPage.tsx
+++ src/pages/AdminPanelPage/ui/AdminPanelPage.tsx
@@ -5,7 +5,7 @@ const AdminPanelPage = () => {
     const { t } = useTranslation('about');
 
     return (
-        <Page>
+        <Page data-testid="AdminPanelPage">
             {t('Админ панель')}
         </Page>
     );
--- src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
+++ src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
@@ -9,7 +9,7 @@ import {
     Article, ArticleView, ArticleSortField, ArticleType,
 } from '@/entities/Article';
 import { ARTICLES_VIEW_LOCALSTORAGE_KEY } from '@/shared/const/localStorage';
-import { SortOrder } from '@/shared/types';
+import { SortOrder } from '@/shared/types/sort';
 import { fetchArticlesList } from '../services/fetchArticlesList/fetchArticlesList';
 import { ArticlesPageSchema } from '../types/articlesPageSchema';
 

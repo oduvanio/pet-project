cbb56c282a780e38e69593392e75e10690c0a65f
oduvanio <oduvanio@mail.ru>
2026-01-29 19:36:17 +0400

    add tests for isolated components with cypress

--- cypress/e2e/common/routing.cy.ts
+++ cypress/e2e/common/routing.cy.ts
@@ -1,4 +1,4 @@
-import { selectByTestId } from 'cypress/helpers/selectByTestId';
+import { selectByTestId } from '../../helpers/selectByTestId';
 
 describe('Роутинг', () => {
     describe('Пользователь НЕ авторизован', () => {

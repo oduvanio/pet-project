90fb108f2d50831e6ba1700c7dd7397f616b6147
oduvanio <oduvanio@mail.ru>
2026-02-06 18:26:24 +0400

    add json settings

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -13,3 +13,6 @@ export { userReducer, userActions } from './model/slice/userSlice';
 export { UserRole } from './model/consts/userConsts';
 
 export type { UserSchema, User } from './model/types/user';
+
+export { useJsonSettings } from './model/selectors/jsonSettings';
+export { saveJsonSettings } from './model/services/saveJsonSettings';
--- src/shared/lib/hooks/useTheme/useTheme.ts
+++ src/shared/lib/hooks/useTheme/useTheme.ts
@@ -1,16 +1,15 @@
 import { useContext } from 'react';
 import { Theme } from '../../../const/theme';
-import { LOCAL_STORAGE_THEME_KEY } from '../../../const/localStorage';
 import { ThemeContext } from '../../context/ThemeContext';
 
 interface UseThemeResult {
-    toggleTheme: () => void;
+    toggleTheme: (saveAction?: (theme: Theme) => void) => void;
     theme: Theme;
 }
 
 export function useTheme(): UseThemeResult {
     const { theme, setTheme } = useContext(ThemeContext);
-    const toggleTheme = () => {
+    const toggleTheme = (saveAction?: (theme: Theme) => void) => {
         let newTheme: Theme;
         switch (theme) {
             case Theme.DARK:
@@ -28,7 +27,7 @@ export function useTheme(): UseThemeResult {
 
         setTheme?.(newTheme);
         document.body.className = newTheme;
-        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+        saveAction?.(newTheme);
     };
 
     return {
